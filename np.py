d1d4126dc225dfc9e4f2b09d6e63520701b69935
Raikia <raikiasec@gmail.com>
2023-01-13 04:29:12 +0000

    Added nmap host searching

--- np.py
+++ np.py
@@ -2,13 +2,14 @@
 
 ### Author
 
-# Chris King (@raikiasec) - chris.king@mandiant.com
+# Chris King (@raikiasec) - raikia@protonmail.com
 
 ### Description
 
 # This script exists because I am tired of grepping and awking gnmap scripts. This script
 # parses pipped-in data and extracts either all the online hosts or hosts that have specific
-# ports open.  See examples for more.
+# ports open.  There is also functionality to search the nmap output on a host-by-host basis
+# See examples for more.
 #
 # The script also prints simple stats about the number of hosts parsed out. Those stats are
 # outputted via stderr, so you can still just redirect the data to a file and have your hosts
@@ -23,20 +24,23 @@
 
 ### Examples
 
-## Parse all online hosts
+## Parse gnmap for all online hosts
 # cat scan.gnmap | ./np.py
 
-## Parse all hosts that have port 80 and port 443 open
+## Parse gnmap for all hosts that have port 80 and port 443 open
 # cat scan.gnmap | ./np.py -p 80 -p 443
 
-## Parse all hosts that have port 80 or port 443 open
+## Parse gnmap for all hosts that have port 80 or port 443 open
 # cat scan.gnmap | ./np.py -p 80 -p 443 -o
 
-## Parse all online hosts and save to a file
+## Parse gnmap for all online hosts and save to a file
 # cat scan.gnmap | ./np.py > onlinehosts.txt
 
-## If the DNS hostname exists in the scan, output that instead of the IP
+## If the DNS hostname exists in the gnmap scan, output that instead of the IP
 # cat scan.gnmap | ./np.py -d
+
+## Parse nmap output for SWEET32 vulnerability
+# nmap -p 443 --script ssl-enum-ciphers example.com | ./np.py -s SWEET32
 
 import argparse
 import sys
@@ -49,10 +53,11 @@ def parse_args():
     parser.add_argument("-o", "--or", dest="multi", action="store_true", help="Search for any port instead of all ports")
     parser.add_argument("-d", "--dns", action="store_true", help="Use DNS name if in the output")
     parser.add_argument("-q", "--quiet", action="store_true", help="Dont print any statuses to stderr")
+    parser.add_argument("-s", "--search", action="append", help="Search the provided nmap output for hosts containing something")
     args = parser.parse_args()
     return args
 
-def print_data(line, args):
+def print_gnmap_host(line, args):
     parts = line.split()
     if len(parts) > 3:
         printable = parts[1]
@@ -60,24 +65,47 @@ def print_data(line, args):
             printable = parts[2].rstrip(')').lstrip('(')
         print(printable)
 
+def print_nmap_host(line, args):
+    line = line.replace("Nmap scan report for ", "")
+    parts = line.split()
+    printable = parts[0]
+    if len(parts) > 1 and not args.dns:
+        printable = parts[1].replace("(","").replace(")","")
+    print(printable)
+
+
 def print_status(line, args):
     if not args.quiet:
         print(" [~] {}".format(line), file=sys.stderr)
 
-def main():
-    args = parse_args()
-    if os.isatty(0):
-        print("You should pipe a gnmap file into this script.  Try \"cat file.gnmap | np\" or something")
-        sys.exit(1)
+def nmap_search(args):
+    print_status("Searching nmap output for {}{}".format("any of: " if args.multi else "",", ".join(args.search)), args)
+    current_host = ""
+    numHosts = 0
+    for line in sys.stdin:
+        if "Nmap scan report for" in line:
+            countOfFindings = 0
+            for s in args.search:
+                if s in current_host:
+                    countOfFindings += 1
+            if (args.multi and countOfFindings > 0) or (not args.multi and countOfFindings == len(args.search)):
+                print_nmap_host(current_host.split("\n")[0], args)
+                numHosts += 1
+            current_host = ""
+        current_host += line
+    print_status("Found {} hosts".format(numHosts), args)
+
+
+def gnmap_search(args):
     searchFor = ["Status: Up"]
     if args.port is not None:
         searchFor = []
         for port in args.port:
             searchFor.append(" {}/open".format(port))
         modifier = ""
-        print_status("Parsing for hosts with {}open ports: {}".format("any " if args.multi else "", ",".join(args.port)), args)
+        print_status("Parsing gnmap for hosts with {}open ports: {}".format("any " if args.multi else "", ",".join(args.port)), args)
     else:
-        print_status("Parsing for online hosts...", args)
+        print_status("Parsing gnmap for online hosts...", args)
 
     found = 0
     for line in sys.stdin:
@@ -87,9 +115,19 @@ def main():
                 numTerms += 1
         if (args.multi and numTerms > 0) or (not args.multi and numTerms == len(searchFor)):
             found += 1
-            print_data(line, args)
+            print_gnmap_host(line, args)
 
     print_status("Found {} hosts".format(found), args)
+
+def main():
+    args = parse_args()
+    if os.isatty(0):
+        print("You should pipe an nmap command or gnmap file into this script.  Try \"cat file.gnmap | np\" or something")
+        sys.exit(1)
+    if args.search is None:
+        gnmap_search(args)
+    else:
+        nmap_search(args)
 
 
 
